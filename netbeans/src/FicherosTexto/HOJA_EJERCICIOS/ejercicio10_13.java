133be4ba57edd0d513dc4a50ff381d6a3fa78da5
dilan <dilan.santacruz@educa.madrid.org>
2026-04-29 12:00:07 +0200

    cambio

--- netbeans/src/FicherosTexto/HOJA_EJERCICIOS/ejercicio10_13.java
+++ netbeans/src/FicherosTexto/HOJA_EJERCICIOS/ejercicio10_13.java
@@ -4,10 +4,40 @@
  */
 package FicherosTexto.HOJA_EJERCICIOS;
 
-/**
- *
- * @author disacr127
- */
+import java.util.*;
+import java.io.*;
+
 public class ejercicio10_13 {
-    
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce el nombre del fichero:");
+        String nombre = sc.nextLine();
+
+        if (nombre.isEmpty()) {
+            nombre = "prueba.txt";
+        }
+
+        BufferedReader in = null;
+
+        try {
+            in = new BufferedReader(new FileReader(nombre));
+
+            System.out.println(in.readLine());
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 }
